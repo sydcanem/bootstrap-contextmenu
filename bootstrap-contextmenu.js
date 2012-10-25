8c692c87ec4e257856d0c4ff7e5ac35aa47a4836
sydcanem <icqhv.santos@gmail.com>
2012-10-25 11:03:30 +0800

    bootstrap-contextmenu.js

---
+++ bootstrap-contextmenu.js
@@ -0,0 +1,141 @@
+/*!
+ * Bootstrap Context Menu
+ * Version: 1.0
+ * A small variation of the dropdown plugin by twitter bootstrap
+ * Twitter Bootstrap (http://twitter.github.com/bootstrap).
+ */
+
+/* =========================================================
+ * http://twitter.github.com/bootstrap/
+ * =========================================================
+ * Copyright 2012 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ========================================================= */
+
+!(function($) {
+
+  "use strict"; // jshint ;_;
+
+  /* CONTEXTMENU CLASS DEFINITION
+   * ============================ */
+
+  var toggle = '[data-toggle=context]'
+    , ContextMenu = function (element) {
+      var $el = $(element).on('contextmenu.context.data-api', this.toggle);
+          $('html').on('click.context.data-api', function () {
+              $el.find('#context-menu').removeClass('open');
+          });
+      }
+
+    ContextMenu.prototype = {
+
+      constructor: ContextMenu
+
+      ,toggle: function(e) {
+
+        var $this = $(this)
+          , $menu
+          , $contextmenu;
+
+        if ($this.is('.disabled, :disabled')) return;
+
+        $menu = getMenu($this);
+        $menu.removeClass('open');
+
+        $contextmenu = $this.find('#context-menu');
+
+        if (!$contextmenu.length) {
+            var tp = getPosition(e, $this, $menu);
+            $menu.attr('style', '')
+                  .css(tp)
+                  .addClass('open');
+            $this.append($menu);
+        } else {
+            var tp = getPosition(e, $this, $menu);
+            $menu.attr('style', '')
+                  .css(tp)
+                  .toggleClass('open');
+        }
+
+        return false;
+      }
+
+    }
+
+    function getMenu($this) {
+      var selector = $this.attr('data-target')
+        , $menu;
+
+      if (!selector) {
+          selector = $this.attr('href')
+          selector = selector && selector.replace(/.*(?=#[^\s]*$)/, '') //strip for ie7
+      }
+
+      $menu = $(selector);
+
+      return $menu;
+    }
+
+    function getPosition(e, $this, $menu) {
+      var posX = e.pageX - $this[0].offsetLeft
+        , posY = e.pageY - $this[0].offsetTop
+        , boundsX = $this.width()
+        , boundsY = $this.height()
+        , menuWidth = $menu.find('.dropdown-menu').outerWidth()
+        , menuHeight = $menu.find('.dropdown-menu').outerHeight()
+        , tp = {}
+        , Y, X;
+
+        if (posY + menuHeight > boundsY) {
+          Y = {"bottom": (boundsY - posY) + menuHeight};
+        } else {
+          Y = {"top": posY};
+        }
+
+        if (posX + menuWidth > boundsX) {
+          X = {"right": ( boundsX - posX ) + menuWidth};
+        } else {
+          X = {"left": posX};
+        }
+
+        return $.extend(tp, Y, X);
+    }
+
+    function clearMenus() {
+      getMenu($(toggle))
+        .removeClass('open');
+    }
+
+  /* CONTEXT MENU PLUGIN DEFINITION
+   * ========================== */
+
+    $.fn.contextmenu = function (option) {
+      return this.each(function () {
+         var $this = $(this)
+          , data = $this.data('contextmenu');
+          if (!data) $this.data('contextmenu', (data = new ContextMenu(this)));
+          if (typeof option == 'string') data[option].call($this);
+      });
+    }
+
+    $.fn.contextmenu.Constructor = ContextMenu;
+
+  $(function () {
+    $('html')
+      .on('click.context.data-api', clearMenus)
+    $('body')
+      .on('contextmenu.context.data-api', toggle, ContextMenu.prototype.toggle);
+  });
+
+}(window.jQuery));
